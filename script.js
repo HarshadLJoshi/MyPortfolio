57d5c29b1b794084374c6d2fea63ab3391623015
HARSHAD JOSHI <harshadjoshi1233@gmail.com>
2023-02-12 14:38:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".nav");
+    nav.classList.toggle("open");
+    if (nav.classList.contains("open")) {
+        nav.style.maxHeight = nav.scrollHeight + "px";
+    }
+    else {
+        nav.removeAttribute("style");
+    }
+} 
